61bf6e3bcbfd177361641c6d72e49353c240a2c4
Maria Suarez <msuarezh@gmail.com>
2023-02-22 19:45:15 +0100

    iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -46,7 +46,28 @@ getInstruction('mashedPotatoes', 0, (step1) => {
 },(error) => console.log(error));
 
 // Iteration 2 - using promises
-// ...
+obtainInstruction('steak', 0)
+  .then( (step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+    return obtainInstruction('steak', 1)
+  })
+  .then( (step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+    return obtainInstruction('steak', 2)
+  })
+  .then( (step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+    return obtainInstruction('steak', 3)
+  })
+  .then( (step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+    return obtainInstruction('steak', 4)
+  })
+  .then( (step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+    document.querySelector("#steak").innerHTML += `<li>Your steak is ready!</li>`
+  })
+  .catch ((error) => console.log(error))
 
 // Iteration 3 using async/await
 // ...
